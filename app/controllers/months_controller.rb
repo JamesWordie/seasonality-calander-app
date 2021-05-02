6a9a9b1a9451761bd78055990ca97c209ece44d0
James Wordie <james.wordie@yahoo.com>
2021-05-02 13:49:29 +0100

    working on the index page for the months as bing the homepage

--- app/controllers/months_controller.rb
+++ app/controllers/months_controller.rb
@@ -1,2 +1,15 @@
 class MonthsController < ApplicationController
+  def index
+    @months = Month.all
+  end
+
+  def show
+    @month = Month.find(params[:id])
+  end
+
+  private
+
+  def strong_params
+    params.require(:month).permit(:name, :season)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'months#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :ingredients, only: :show
+  resources :ingredients, only: [:show, :index]
   resources :months, only: :show
   resources :category, only: :show
 
