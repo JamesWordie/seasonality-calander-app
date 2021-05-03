cf5a6a3048050c5fe29298a44cf30621b4d27fad
James Wordie <james.wordie@yahoo.com>
2021-05-03 20:37:27 +0100

    added multisearch, setup to continue

--- app/models/month.rb
+++ app/models/month.rb
@@ -3,13 +3,13 @@ class Month < ApplicationRecord
   has_many :ingredients, through: :calanders
 
   include PgSearch::Model
-  pg_search_scope :search_by_ingredient_name,
-                  against: :name,
-                  associated_against: {
-                    ingredient: :name,
-                    category: :name
-                  },
-                  using: {
-                    tsearch: { prefix: true }
-                  }
+  # pg_search_scope :search_by_ingredient_name,
+  #                 against: :name,
+  #                 associated_against: {
+  #                   ingredient: :name,
+  #                   category: :name
+  #                 },
+  #                 using: {
+  #                   tsearch: { prefix: true }
+  #                 }
 end
