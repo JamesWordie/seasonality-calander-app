ade252d22a45a6e2e8dd57bd7c3cbc6fb20c8fc5
James Wordie <james.wordie@yahoo.com>
2021-05-04 07:16:02 +0100

    the pg search multisearch now works for search's against an ingredient name, does not work at present against category search

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,20 @@
 class IngredientsController < ApplicationController
+  def index
+    @months = Month.all
+    if params[:query].present?
+      @ingredients = PgSearch.multisearch(params[:query])
+    else
+      @ingredients = Ingredient.all
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  private
+
+  def strong_params
+    params.require(:ingredient).permit(:name)
+  end
 end
--- app/models/month.rb
+++ app/models/month.rb
@@ -2,14 +2,4 @@ class Month < ApplicationRecord
   include PgSearch::Model
   has_many :calanders
   has_many :ingredients, through: :calanders
-
-  # pg_search_scope :search_by_ingredient_name,
-  #                 against: :name,
-  #                 associated_against: {
-  #                   ingredient: :name,
-  #                   category: :name
-  #                 },
-  #                 using: {
-  #                   tsearch: { prefix: true }
-  #                 }
 end
