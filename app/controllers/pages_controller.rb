02074cd495039526dd3f08d30f1e5773b6bf8da6
James Wordie <james.wordie@yahoo.com>
2021-05-12 20:55:33 +0100

    added the homepage so that you open on the current month displaying whats in season, then go to view whole calander, ingredients etc from there

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: :home
 
   def home
+    @month = Month.find_by(name: Date.today.strftime('%B'))
+    @categories = Category.all
   end
 end
